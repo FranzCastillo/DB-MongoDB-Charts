c8ab97e36015dfbce160f651f729840a6b0d9aed
Francisco Castillo <castillocerna@gmail.com>
2024-02-10 20:58:50 -0600

    All CSV Reads Implemented

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import csv
 import codecs
+import random
 
+from faker import Faker
 from src.connections.atlas import AtlasConnection
 
 
@@ -11,7 +13,7 @@ def main():
         'countries': get_countries_documents(),
         'universities': get_universities_documents(),
         'students': get_students_documents(),
-        # 'exchanges': None,
+        'exchanges': get_exchanges_documents(),
     }
 
     for collection_name, documents in collections.items():
@@ -97,6 +99,88 @@ def get_students_documents():
             }
             documents.append(student)
     return documents
+
+
+def get_exchanges_documents():
+    documents = []
+    with codecs.open('src/data/exchanges.csv', 'r', encoding='utf-8', errors='ignore') as file:
+        exchanges = csv.DictReader(file)
+        for row in exchanges:
+            exchange = {
+                "_id": row["_id"],
+                "student_id": row["student_id"],
+                "university_id": row["university_id"],
+                "details": {
+                    "year": row["year"],
+                    "semester": row["semester"],
+                    "modality": row["modality"],
+                    "status": row["status"],
+                    "start_date": row["start_date"],
+                    "end_date": row["end_date"],
+                    "comments": row["comments"],
+                }
+            }
+            documents.append(exchange)
+    return documents
+
+
+# Used to mock exchange documents
+
+def mock_exchanges(num_documents=1000):
+    fake = Faker()
+    documents = []
+    student_ids = get_student_ids()
+    university_ids = get_university_ids()
+    modalities = ["ELAP", "Beca", "Convenio", "Visitante"]
+    statuses = ["En curso", "Finalizada", "Cancelada"]
+    start_date = fake.date_between(start_date='-10y', end_date='today')
+    end_date = fake.date_between(start_date=start_date, end_date='+1y')
+
+    for i in range(num_documents):
+        document = {
+            "_id": i,
+            "student_id": random.choice(student_ids),
+            "university_id": random.choice(university_ids),
+            "details": {
+                "year": random.randint(2000, 2024),
+                "semester": random.randint(1, 2),
+                "modality": random.choice(modalities),
+                "status": random.choice(statuses),
+                "start_date": start_date.strftime("%Y-%m-%d"),
+                "end_date": end_date.strftime("%Y-%m-%d"),
+                "comments": fake.text(max_nb_chars=200),
+            }
+        }
+        documents.append(document)
+
+    return documents
+
+
+def get_student_ids():
+    with codecs.open('src/data/students.csv', 'r', encoding='utf-8', errors='ignore') as file:
+        students = csv.DictReader(file)
+        return [int(row["_id"]) for row in students]
+
+
+def get_university_ids():
+    with codecs.open('src/data/universities.csv', 'r', encoding='utf-8', errors='ignore') as file:
+        universities = csv.DictReader(file)
+        return [int(row["_id"]) for row in universities]
+
+
+def parse_exchanges(documents):
+    # Parses the documents to a csv file
+    with open('exchanges.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["_id", "student_id", "university_id", "year", "semester", "modality", "status", "start_date",
+                         "end_date", "comments"])
+        for document in documents:
+            writer.writerow([document["_id"], document["student_id"], document["university_id"],
+                             document["details"]["year"], document["details"]["semester"],
+                             document["details"]["modality"],
+                             document["details"]["status"], document["details"]["start_date"],
+                             document["details"]["end_date"],
+                             document["details"]["comments"]])
 
 
 if __name__ == "__main__":
